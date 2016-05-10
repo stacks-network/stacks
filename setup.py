4176139f36cc0ef46c8061cbd9325b8b7334f11e
Muneeb Ali <muneeb@ali.vc>
2016-05-10 18:48:53 -0400

    added back setup.py and bumped version

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Blockstack:
+
+    This installs blockstack-server and blockstack-cli
+"""
+
+from setuptools import setup, find_packages
+import sys
+import os
+
+setup(
+    name='blockstack',
+    version='0.0.10.7',
+    url='https://github.com/blockstack/blockstack',
+    license='GPLv3',
+    author='Blockstack.org',
+    author_email='support@blockstack.org',
+    description='Decentralized naming and storage',
+    keywords='blockchain bitcoin btc cryptocurrency name domain naming system data',
+    download_url='https://github.com/blockstack/blockstack/archive/master.zip',
+    zip_safe=False,
+    install_requires=[
+        'blockstore-client==0.0.12.7',
+        'blockstore==0.0.10.6',
+        'registrar==0.0.3.4'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
